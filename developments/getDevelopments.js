d02f87375419b3612619d8b9fdaa84a27bea3858
UserUndefined <cristov.igor@gmail.com>
2017-01-14 15:22:09 +0000

    Update the mock data

--- developments/getDevelopments.js
+++ developments/getDevelopments.js
@@ -9,11 +9,57 @@ module.exports.index = function(event, context, callback){
         "Access-Control-Allow-Origin" : "*" // Required for CORS support to work
     },
     body: JSON.stringify([
-        {
-          name: 'Development One',
-            date: moment.now()
-        }
-      ]
+            {
+                name: 'Example House',
+                location: 'Leeds',
+                vendor: 'Midas',
+                agent: 'BPS',
+                approved: 'Y',
+                launchDate: '17 Jan 2017',
+                plotNumber: '20',
+                legalCompletion: 'Y'
+            },
+            {
+                name: 'Little House',
+                location: 'Manchester',
+                vendor: 'Trent',
+                agent: 'BPS',
+                approved: 'Y',
+                launchDate: '01 Feb 2017',
+                plotNumber: '5',
+                legalCompletion: 'Y'
+            },
+            {
+                name: 'Mighty House',
+                location: 'Sheffield',
+                vendor: 'Trent',
+                agent: 'PLL',
+                approved: 'Y',
+                launchDate: '18 Feb 2017',
+                plotNumber: '9',
+                legalCompletion: 'N'
+            },
+            {
+                name: 'Farm House',
+                location: 'Manchester',
+                vendor: 'Midas',
+                agent: 'PLL',
+                approved: 'Y',
+                launchDate: '29 Mar 2017',
+                plotNumber: '25',
+                legalCompletion: 'Y'
+            },
+            {
+                name: 'White House',
+                location: 'Sheffield',
+                vendor: 'Trent',
+                agent: 'BPS',
+                approved: 'Y',
+                launchDate: '30 Mar 2017',
+                plotNumber: '10',
+                legalCompletion: 'N'
+            }
+        ]
     )
   };
 
